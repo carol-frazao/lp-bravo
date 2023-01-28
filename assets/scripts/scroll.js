4b48742e311583980a84db0652a9cee08bbf7719
Carol Frazão <94201597+carol-frazao@users.noreply.github.com>
2023-01-27 21:28:34 -0300

    Adicionar sessão RCPM

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -1,9 +1,9 @@
 const posicaoInicial = (section) => $(section).position().top;
 
 const scrollar = (section) => {
-  var target_offset = $(section).offset();
-  var target_top = target_offset.top;
-  $("html, body").animate({ scrollTop: target_top - 60 }, 0);
+  const target_offset = $(section).offset();
+  const target_top = target_offset.top;
+  $("html, body").animate({ scrollTop: target_top - 60}, 0);
 };
 
 $(document).scroll(function () {
